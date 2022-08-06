b49cc8d36162bf6c6e0662a622c1fe728e5e070b
Kyun-Seop Bae <k@acr.kr>
2022-08-06 04:50:02 +0000

    version 0.2.2

--- R/pow2x2mse.R
+++ R/pow2x2mse.R
@@ -1,7 +1,7 @@
 pow2x2mse = function(n1, n2, mse, True.R=1, Alpha=0.1, ThetaL=0.8, ThetaU=1.25)
 {
 # n1, n2: sample size for each group
-  if (True.R <= ThetaL | True.R >= ThetaU) return(0)
+#  if (True.R <= ThetaL | True.R >= ThetaU) return(0)
   t0 = qt(1 - Alpha/2, n1 + n2 - 2)
   p1 = pt(-1*t0, n1 + n2 - 2, ncp = log(ThetaL/True.R) / sqrt(mse/2*(1/n1 + 1/n2)) ) # 1st beta error: LL > 0.8 = 1 - p1
   p2 = pt(t0, n1 + n2 - 2, ncp = log(ThetaU/True.R) / sqrt(mse/2*(1/n1 + 1/n2)) )    # 2nd beta error: UL < 1.25 = p2
--- R/powmse.R
+++ R/powmse.R
@@ -1,6 +1,6 @@
 powmse = function(n, mse, DesignNo=1, True.R=1, Alpha=0.1, ThetaL=0.8, ThetaU=1.25)
 {
-  if (True.R <= ThetaL | True.R >= ThetaU) return(0)
+#  if (True.R <= ThetaL | True.R >= ThetaU) return(0)
 
   aNu0 = c(2, 4, 4, 6, 12, 12, 12, 12, 12)
   aNu1 = c(2, 3, 4, 5, 5, 6, 4, 9, 6)
@@ -10,8 +10,8 @@ powmse = function(n, mse, DesignNo=1, True.R=1, Alpha=0.1, ThetaL=0.8, ThetaU=1.
   bx = sqrt(mse*aBx[DesignNo]/n)
   t0 = qt(1 - Alpha/2, nu)
   p1 = pt(-t0, nu, ncp = log(ThetaL/True.R)/bx) # 1st beta error: LL > 0.8 = 1 - p1
-  p2 = pt(t0, nu, ncp = log(ThetaU/True.R)/bx)    # 2nd beta error: UL < 1.25 = p2
-  Power = p1 - p2  # 1 - (1 - p1 + p2) = 1 - both beta errors
+  p2 = pt(t0, nu, ncp = log(ThetaU/True.R)/bx)  # 2nd beta error: UL < 1.25 = p2
+  Power = p1 - p1*p2  # = 1 - (1 - p1 + p2 - (1 - p1)*p2) = 1 - both beta errors
   if (Power < 0) Power = 0
   return(Power)
 }
